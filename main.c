9a624d668500d27ad8d8d88f96b8e997b4fae85a
kuili <55239610@qq.com>
2021-01-07 12:05:41 +0800

    add its app

--- main.c
+++ main.c
@@ -1,11 +1,11 @@
-#include <itr.h>
+#include <its.h>
 
 int main(int argc, char *argv[])
 {
-    const char *appid = "5d2f27d2";
-    const char *apikey = "a8331910d59d41deea317a3c76d47b60";
-    const char *apisecret = "8110566cd9dd13066f9a1e38aeb12a48";
+    const char *appid = "xxxxxxxx";
+    const char *apikey = "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx";
+    const char *apisecret = "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx";
 
-    getITRResult(appid, apikey, apisecret, "../Res/itr.png");
+    getITSResult(appid, apikey, apisecret, "cn", "en", "欢迎来到比特人生的世界");
     return 0;
 }
